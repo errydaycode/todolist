ffa6b9b816f5948aaf439c92859e9df715187f07
listopadgeorgii <listopadiogl@gmail.com>
2023-01-19 23:55:14 +0300

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
 function App() {
 
-    const tasks = [
+    let tasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
-        { id: 3, title: "Redux", isDone: false }
+        { id: 4, title: "Redux", isDone: false }
     ]
 
+
+
     function removeTask(id: number) {
+        tasks = tasks.filter( t =>  t.id !== id)
         debugger
-        let resultTasks = tasks.filter( () => {return true} )
-        console.log(resultTasks)
     }
 
 
