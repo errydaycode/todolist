9cee7260ea41e03a641e07b37443cf073022aedb
listopadgeorgii <listopadiogl@gmail.com>
2023-01-20 00:34:45 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
+
+
 function App() {
 
-    let tasks = [
+    let initTasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
         { id: 4, title: "Redux", isDone: false }
     ]
 
-
+    let arr = useState(initTasks);
+    let tasks =  arr[0]
+    let setTasks = arr[1]
 
     function removeTask(id: number) {
-        tasks = tasks.filter( t =>  t.id !== id)
-        debugger
+        let filteredTasks = tasks.filter( t =>  t.id !== id)
+        setTasks(filteredTasks)
     }
 
 
